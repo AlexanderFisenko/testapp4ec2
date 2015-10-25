0af4b54a2ae09c3eb47f05ff372567c87adf5e60
Alexander Fisenko <1alexanderfisenko@gmail.com>
2015-10-25 16:17:53 +0500

    dsd

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,10 +35,10 @@ set :default_env, path: '~/.rbenv/shims:~/.rbenv/bin:$PATH'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -57,4 +57,12 @@ namespace :deploy do
     end
   end
 
+end
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:legacy_restart'
+  end
 end
